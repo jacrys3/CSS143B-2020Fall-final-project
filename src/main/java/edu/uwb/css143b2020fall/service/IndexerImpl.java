1965733534abaca8580d3d2025d7996185783101
Jacrys3 <ryse0528@gmail.com>
2020-11-30 19:24:36 -0800

    testing stuff out on index

--- src/main/java/edu/uwb/css143b2020fall/service/IndexerImpl.java
+++ src/main/java/edu/uwb/css143b2020fall/service/IndexerImpl.java
@@ -11,18 +11,29 @@ import java.util.Map;
 public class IndexerImpl implements Indexer {
     public Map<String, List<List<Integer>>> index(List<String> docs) {
         Map<String, List<List<Integer>>> indexes = new HashMap<>();
-        ArrayList<String> words = new ArrayList<>();
+        List<List<Integer>> pos = new ArrayList<>();
+        List<Integer> tmp = new ArrayList<>();
+        //clearList(tmp, docs.size());
 
         for (int i = 0; i < docs.size(); i++) {
             String str = docs.get(i);
-            String[] data = str.split(" ");
-            for(int k = 0; k < data.length; k++)
-                words.add(data[k]);
+            String[] title = str.split(" ");
+            for(int k = 0; k < title.length; k++){
+                if(indexes.get(title[k]) != null){
+                    pos = indexes.get(title[k]);
+                    tmp = pos.get(i);
+                    tmp.add(k);
+                    indexes.replace(title[k], pos);
+                } else{
+                    tmp = new ArrayList<>();
+                    tmp.add(k);
+                    pos.add(tmp);
+                    indexes.put(title[k], pos);
+                }
+            }
+            //try to have separate for loops for adding to the individual doc lists and one for adding those lists to the hashmap
         }
 
-        for (int i = 0; i < words.size(); i++) {
-            System.out.println(words.get(i));
-        }
         return indexes;
     }
 }
