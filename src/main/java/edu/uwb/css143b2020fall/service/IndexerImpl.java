cae34d5a4f151de2bfa638879c18a16c6da1d66b
Jacrys3 <ryse0528@gmail.com>
2020-12-03 14:38:39 -0800

    Tried new method, kinda works for some of the words

--- src/main/java/edu/uwb/css143b2020fall/service/IndexerImpl.java
+++ src/main/java/edu/uwb/css143b2020fall/service/IndexerImpl.java
@@ -2,16 +2,13 @@ package edu.uwb.css143b2020fall.service;
 
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Service
 public class IndexerImpl implements Indexer {
     public Map<String, List<List<Integer>>> index(List<String> docs) {
         Map<String, List<List<Integer>>> indexes = new HashMap<>();
-        Map<String, Integer> words = new HashMap<>();
+        Map<String, String> words = new HashMap<>();
 
         for(int i = 0; i < docs.size(); i++){ // adds all the words from the docs to a map
             String str = docs.get(i);
@@ -20,8 +17,27 @@ public class IndexerImpl implements Indexer {
                 if(words.get(title[j]) != null){
                     continue;
                 }
-                words.put(title[j], 0);
+                words.put(title[j], title[j]);
             }
+        }
+
+        Collection<String> wordList = words.values();
+        Object[] word = wordList.toArray();
+
+        for (int i = 0; i < word.length; i++) {
+            List<List<Integer>> pos = new ArrayList<>();
+            for (int j = 0; j < docs.size(); j++) {
+                String str = docs.get(j);
+                String[] line = str.split(" ");
+                List<Integer> doc = new ArrayList<>();
+                for (int k = 0; k < line.length; k++) {
+                    if (word[i].equals(line[k])) {
+                        doc.add(k);
+                    }
+                }
+                pos.add(doc);
+            }
+            indexes.put((String) word[i], pos);
         }
 
         /*for (int i = 0; i < docs.size(); i++) { //goes through each doc
