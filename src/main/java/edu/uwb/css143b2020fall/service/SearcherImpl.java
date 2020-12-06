334b21b83ff1917342e81afd659c1ee7356c4060
Jacrys3 <ryse0528@gmail.com>
2020-12-06 13:44:42 -0800

    Search completely clears all tests except for when more than two words are search at the same time. need to check order of indexes

--- src/main/java/edu/uwb/css143b2020fall/service/SearcherImpl.java
+++ src/main/java/edu/uwb/css143b2020fall/service/SearcherImpl.java
@@ -10,7 +10,54 @@ import java.util.Map;
 public class SearcherImpl implements Searcher {
     public List<Integer> search(String keyPhrase, Map<String, List<List<Integer>>> index) {
         List<Integer> result = new ArrayList<>();
-        // add your code
-        return result;
+        boolean isRealWord = true;
+        String[] words = keyPhrase.split(" ");
+
+        for (int i = 0; i < words.length; i++) {
+            List<Integer> docIndexes = getDocs(words[i], index);
+            if (docIndexes == null || docIndexes.size() == 0) {
+                isRealWord = false;
+            } else if (words.length == 1) {
+                return docIndexes;
+            } else {
+                isRealWord = true;
+            }
+        }
+        if (!isRealWord) {
+            return new ArrayList<>();
+        }
+        List<Integer> allWordsGood = new ArrayList<>();
+        for (int i = 0; i < words.length - 1; i++) {
+            allWordsGood = compareDocs(getDocs(words[i], index), getDocs(words[i + 1], index));
+        }
+        return allWordsGood;
+    }
+
+    private List<Integer> getDocs(String word, Map<String, List<List<Integer>>> source) {
+        List<Integer> indexes = new ArrayList<>();
+
+        List<List<Integer>> tmp = source.get(word);
+        if (tmp != null) {
+            for (int i = 0; i < tmp.size(); i++) {
+                List<Integer> temp = tmp.get(i);
+                if (!temp.isEmpty())
+                    indexes.add(i);
+            }
+        }
+
+        return indexes;
+    }
+
+    private List<Integer> compareDocs(List<Integer> l1, List<Integer> l2) {
+        List<Integer> combined = new ArrayList<>();
+        for (int i = 0; i < l1.size(); i++) {
+            for (int j = 0; j < l2.size(); j++) {
+                if (l1.get(i) == l2.get(j)) {
+                    combined.add(l1.get(i));
+                }
+            }
+        }
+
+        return combined;
     }
 }
