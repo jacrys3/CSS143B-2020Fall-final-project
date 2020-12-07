1ba96460ceae6165817cf7bbec36888fffe0b94c
Jacrys3 <ryse0528@gmail.com>
2020-12-06 23:16:43 -0800

    trying to fix one test case with three words in SearcherImpl

--- src/main/java/edu/uwb/css143b2020fall/service/IndexerImpl.java
+++ src/main/java/edu/uwb/css143b2020fall/service/IndexerImpl.java
@@ -9,40 +9,41 @@ public class IndexerImpl implements Indexer {
     public Map<String, List<List<Integer>>> index(List<String> docs) {
         Map<String, List<List<Integer>>> indexes = new HashMap<>();
         Map<String, String> words = new HashMap<>();
-
-        for(int i = 0; i < docs.size(); i++){ // this part adds all the different words in the docs into one array
-            // to be used in the next part
-            String str = docs.get(i);
-            String[] title = str.split(" ");
-            for (int j = 0; j < title.length; j++) {
-                if(title[j].equals("")) continue; // skips empty words since more than one space next to each other
-                // create empty words from the split function.
-                if(words.get(title[j]) != null) continue;
-                words.put(title[j], title[j]);
-            }
-        }
-
-        Collection<String> wordList = words.values();
-        Object[] word = wordList.toArray();
-
-        for (int i = 0; i < word.length; i++) { // this part searches for the indexes of the words from the list
-            // and puts them into the list indexes so it can be returned
-            List<List<Integer>> pos = new ArrayList<>();
-            for (int j = 0; j < docs.size(); j++) {
-                String str = docs.get(j);
-                String[] line = str.split(" ");
-                List<Integer> doc = new ArrayList<>();
-                int count = 0;
-                for (int k = 0; k < line.length; k++) {
-                    if (word[i].equals(line[k])) {
-                        doc.add(count);
-                    }
-                    if(!line[k].equals("")) count++; // This is here because split still counts "" as a word and so
-                    // count will not increment if the word is "" because that is not actually a word.
+        if (docs != null) {
+            for (int i = 0; i < docs.size(); i++) { // this part adds all the different words in the docs into one array
+                // to be used in the next part
+                String str = docs.get(i);
+                String[] title = str.split(" ");
+                for (int j = 0; j < title.length; j++) {
+                    if (title[j].equals("")) continue; // skips empty words since more than one space next to each other
+                    // create empty words from the split function.
+                    if (words.get(title[j]) != null) continue;
+                    words.put(title[j], title[j]);
                 }
-                pos.add(doc);
             }
-            indexes.put((String) word[i], pos);
+
+            Collection<String> wordList = words.values();
+            Object[] word = wordList.toArray();
+
+            for (int i = 0; i < word.length; i++) { // this part searches for the indexes of the words from the list
+                // and puts them into the list indexes so it can be returned
+                List<List<Integer>> pos = new ArrayList<>();
+                for (int j = 0; j < docs.size(); j++) {
+                    String str = docs.get(j);
+                    String[] line = str.split(" ");
+                    List<Integer> doc = new ArrayList<>();
+                    int count = 0;
+                    for (int k = 0; k < line.length; k++) {
+                        if (word[i].equals(line[k])) {
+                            doc.add(count);
+                        }
+                        if (!line[k].equals("")) count++; // This is here because split still counts "" as a word and so
+                        // count will not increment if the word is "" because that is not actually a word.
+                    }
+                    pos.add(doc);
+                }
+                indexes.put((String) word[i], pos);
+            }
         }
         return indexes;
     }
